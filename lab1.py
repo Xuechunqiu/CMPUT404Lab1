a9d7dfd8a16904ae38bcdddc85b7546f0cde74a8
xuechun <noglues666@gmail.com>
2021-01-11 22:45:13 -0500

    get raw url source code

--- lab1.py
+++ lab1.py
@@ -5,3 +5,7 @@ print(requests.__version__)
 url = "http://www.google.com/"
 response = requests.get(url)
 print(response.status_code)
+
+url_itself = "https://raw.githubusercontent.com/Xuechunqiu/CMPUT404Lab1/master/lab1.py"
+response = requests.get(url_itself)
+print(response.text)
